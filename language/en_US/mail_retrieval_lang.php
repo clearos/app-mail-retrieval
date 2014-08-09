5202b2f307a94f27e03edc5449d07e0a923fe1f4
clearostranslations <translations@clearfoundation.com>
2014-08-09 00:40:02 -0400

    Prepped for translation server

--- language/en_US/mail_retrieval_lang.php
+++ language/en_US/mail_retrieval_lang.php
@@ -1,19 +1,19 @@
 <?php
 
-$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_apop'] = 'APOP';
 $lang['mail_retrieval_app_description'] = 'The Mail Retrieval app can be used to fetch mail from external POP and IMAP servers.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
+$lang['mail_retrieval_local_username_invalid'] = 'Local username is invalid.';
+$lang['mail_retrieval_local_username'] = 'Local Username';
 $lang['mail_retrieval_mail_entries'] = 'Mail Entries';
 $lang['mail_retrieval_mail_entry'] = 'Mail Entry';
-$lang['mail_retrieval_server'] = 'Server';
-$lang['mail_retrieval_local_username'] = 'Local Username';
-$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
-$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
-$lang['mail_retrieval_pop3'] = 'POP3';
-$lang['mail_retrieval_apop'] = 'APOP';
-$lang['mail_retrieval_imap'] = 'IMAP';
-$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
 $lang['mail_retrieval_mail_server'] = 'Mail Server';
-$lang['mail_retrieval_server_invalid'] = 'Server is invalid.';
+$lang['mail_retrieval_pop3'] = 'POP3';
 $lang['mail_retrieval_protocol_invalid'] = 'Protocol is invalid.';
-$lang['mail_retrieval_local_username_invalid'] = 'Local username is invalid.';
 $lang['mail_retrieval_remote_username'] = 'Remote Username';
+$lang['mail_retrieval_server_invalid'] = 'Server is invalid.';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
