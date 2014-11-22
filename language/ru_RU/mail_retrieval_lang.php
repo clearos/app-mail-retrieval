6fa70362fefb0ddd579dc79173fab34b8e133d7a
clearostranslations <translations@clearfoundation.com>
2014-11-21 19:37:24 -0500

    Updated translations via Pootle

--- language/ru_RU/mail_retrieval_lang.php
+++ language/ru_RU/mail_retrieval_lang.php
@@ -3,19 +3,19 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['mail_retrieval_apop'] = 'APOP';
-$lang['mail_retrieval_app_description'] = 'The Mail Retrieval app can be used to fetch mail from external POP and IMAP servers.';
-$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
-$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_app_description'] = 'Приложение "Получение почты" может использоваться, чтобы получать почту с внешних POP и IMAP-серверов.';
+$lang['mail_retrieval_app_name'] = 'Получение почты';
+$lang['mail_retrieval_auto_detect'] = 'Автоопределение';
 $lang['mail_retrieval_imap'] = 'IMAP';
-$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
-$lang['mail_retrieval_local_username'] = 'Local Username';
-$lang['mail_retrieval_local_username_invalid'] = 'Local username is invalid.';
-$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
-$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
-$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Оставить на удаленном сервере';
+$lang['mail_retrieval_local_username'] = 'Локальный пользователь';
+$lang['mail_retrieval_local_username_invalid'] = 'Локальное имя пользователя - инвалид.';
+$lang['mail_retrieval_mail_entries'] = 'Почтовые записи';
+$lang['mail_retrieval_mail_entry'] = 'Почтовая запись';
+$lang['mail_retrieval_mail_server'] = 'Почтовый сервер';
 $lang['mail_retrieval_pop3'] = 'POP3';
-$lang['mail_retrieval_protocol_invalid'] = 'Protocol is invalid.';
-$lang['mail_retrieval_remote_username'] = 'Remote Username';
-$lang['mail_retrieval_server'] = 'Server';
-$lang['mail_retrieval_server_invalid'] = 'Server is invalid.';
-$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
+$lang['mail_retrieval_protocol_invalid'] = 'Протокол является недействительным.';
+$lang['mail_retrieval_remote_username'] = 'Удаленное имя пользователя';
+$lang['mail_retrieval_server'] = 'Сервер';
+$lang['mail_retrieval_server_invalid'] = 'Сервер неверен.';
+$lang['mail_retrieval_ssl_mode'] = 'Режим SSL';
