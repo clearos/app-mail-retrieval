ae496169e200f70f0d62b6c3185b9d8dfc0522e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Paštas Priimamas programa gali būti naudojama atnešti paštą iš išorės POP ir IMAP serverius.';
+$lang['mail_retrieval_app_name'] = 'paštas Priimamas';
+$lang['mail_retrieval_auto_detect'] = 'Automatiškai aptikti';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Laikyti nuotolinio serverio';
+$lang['mail_retrieval_local_username'] = 'vietinis Nick';
+$lang['mail_retrieval_local_username_invalid'] = 'Vietinis username is negalioja.';
+$lang['mail_retrieval_mail_entries'] = 'paštas įrašai';
+$lang['mail_retrieval_mail_entry'] = 'pašto įrašas';
+$lang['mail_retrieval_mail_server'] = 'pašto serveris';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokolas yra negaliojantis.';
+$lang['mail_retrieval_remote_username'] = 'Nuotolinis Nick';
+$lang['mail_retrieval_server'] = 'serverio';
+$lang['mail_retrieval_server_invalid'] = 'Serveris negalioja.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL režimas';
