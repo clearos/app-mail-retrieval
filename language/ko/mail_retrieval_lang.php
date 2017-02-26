acb139ef24f97c255f18f9b10770ce7e205e4085
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = '메일 검색 앱은 외부 POP 및 IMAP 서버에서 메일을 가져 오는 데 사용할 수 있습니다.';
+$lang['mail_retrieval_app_name'] = '메일 검색';
+$lang['mail_retrieval_auto_detect'] = '자동 감지';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = '원격 서버에 보관';
+$lang['mail_retrieval_local_username'] = '지역 이름';
+$lang['mail_retrieval_local_username_invalid'] = '로컬 사용자 이름이 잘못되었습니다.';
+$lang['mail_retrieval_mail_entries'] = '메일 항목';
+$lang['mail_retrieval_mail_entry'] = '메일 항목';
+$lang['mail_retrieval_mail_server'] = '메일 서버';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = '프로토콜이 잘못되었습니다.';
+$lang['mail_retrieval_remote_username'] = '원격 사용자 이름';
+$lang['mail_retrieval_server'] = '섬기는 사람';
+$lang['mail_retrieval_server_invalid'] = '서버가 잘못되었습니다.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL 모드';
