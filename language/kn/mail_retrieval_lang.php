ed33910c95ad2b9a60a21395b3e9ef1e5df54a9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP ಅನ್ನು';
+$lang['mail_retrieval_app_description'] = 'ಮೇಲ್ ಪುನಃ ಅಪ್ಲಿಕೇಶನ್ ಬಾಹ್ಯ POP ಮತ್ತು IMAP ಸರ್ವರ್ ಮೇಲ್ ಅನ್ನು ಬಳಸಬಹುದು.';
+$lang['mail_retrieval_app_name'] = 'ಮೇಲ್ ಪುನಃ';
+$lang['mail_retrieval_auto_detect'] = 'ಸ್ವಯಂ ಪತ್ತೆ';
+$lang['mail_retrieval_imap'] = 'IMAP ಅನ್ನು';
+$lang['mail_retrieval_keep_on_remote_server'] = 'ದೂರಸ್ಥ ಪರಿಚಾರಕದಲ್ಲಿ ಇರಿಸಿ';
+$lang['mail_retrieval_local_username'] = 'ಸ್ಥಳೀಯ ಬಳಕೆದಾರ ಹೆಸರು';
+$lang['mail_retrieval_local_username_invalid'] = 'ಸ್ಥಳೀಯ ಬಳಕೆದಾರ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_retrieval_mail_entries'] = 'ಮೇಲ್ ನಮೂದುಗಳು';
+$lang['mail_retrieval_mail_entry'] = 'ಮೇಲ್ ಎಂಟ್ರಿ';
+$lang['mail_retrieval_mail_server'] = 'ಮೇಲ್ ಸರ್ವರ್';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ಪ್ರೊಟೊಕಾಲ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_retrieval_remote_username'] = 'ರಿಮೋಟ್ ಬಳಕೆದಾರ ಹೆಸರು';
+$lang['mail_retrieval_server'] = 'ಸರ್ವರ್';
+$lang['mail_retrieval_server_invalid'] = 'ಸರ್ವರ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_retrieval_ssl_mode'] = 'ಎಸ್ಎಸ್ಎಲ್ ಮೋಡ್';
