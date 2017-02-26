611447498f53b25aa684d74e424f16e4040f052c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'મેલ પુનઃપ્રાપ્તિ એપ્લિકેશન બાહ્ય POP અને IMAP સર્વરો મેલ મેળવે માટે વાપરી શકાય છે.';
+$lang['mail_retrieval_app_name'] = 'મેલ પુનઃપ્રાપ્તિ';
+$lang['mail_retrieval_auto_detect'] = 'સ્વતઃ શોધો';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'દૂરસ્થ સર્વર પર રાખો';
+$lang['mail_retrieval_local_username'] = 'સ્થાનિક વપરાશકર્તા નામ';
+$lang['mail_retrieval_local_username_invalid'] = 'સ્થાનિક વપરાશકર્તા નામ અમાન્ય છે.';
+$lang['mail_retrieval_mail_entries'] = 'મેઇલ પ્રવેશો';
+$lang['mail_retrieval_mail_entry'] = 'મેઇલ એન્ટ્રી';
+$lang['mail_retrieval_mail_server'] = 'મેલ સર્વર';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'પ્રોટોકોલ અમાન્ય છે.';
+$lang['mail_retrieval_remote_username'] = 'દૂરસ્થ વપરાશકર્તા નામ';
+$lang['mail_retrieval_server'] = 'સર્વર';
+$lang['mail_retrieval_server_invalid'] = 'સર્વર અમાન્ય છે.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL સ્થિતિ';
