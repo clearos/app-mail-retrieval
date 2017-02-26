8721be9c79a04a0082da000ce09a655d695dedfe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplikacija Mail za preuzimanje može se koristiti za dohvaćanje e-pošte iz vanjskih POP i IMAP poslužitelja.';
+$lang['mail_retrieval_app_name'] = 'mail za preuzimanje';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Imajte na udaljenom poslužitelju';
+$lang['mail_retrieval_local_username'] = 'Lokalno Korisničko ime';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokalno korisničko ime je nevažeći.';
+$lang['mail_retrieval_mail_entries'] = 'mail tekstova';
+$lang['mail_retrieval_mail_entry'] = 'mail Ulaz';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol nije valjan.';
+$lang['mail_retrieval_remote_username'] = 'Daljinski Korisničko ime';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server je nevažeći.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL načina rada';
