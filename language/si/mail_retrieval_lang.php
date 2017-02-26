6fd7c275bd1903a45c0cd77c2fe814b9fe4022d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:33:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'ඇති තැපැල් නැගී සිටීමේ යෙදුම බාහිර POP හා IMAP සේවාදායක තැපැල් බෝගයන්ගේ කිරීම සඳහා භාවිතා කල හැක.';
+$lang['mail_retrieval_app_name'] = 'තැපැල් නැගී සිටීමේ';
+$lang['mail_retrieval_auto_detect'] = 'වාහන හදුනාගන්න';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'දුරස්ථ සේවාදායකය සිටින්න';
+$lang['mail_retrieval_local_username'] = 'දේශීය පරිශීලක නාමය';
+$lang['mail_retrieval_local_username_invalid'] = 'දේශීය පරිශීලක නාමය වලංගු නැත.';
+$lang['mail_retrieval_mail_entries'] = 'තැපැල් අයැදුම්පත්';
+$lang['mail_retrieval_mail_entry'] = 'තැපැල් සටහන්';
+$lang['mail_retrieval_mail_server'] = 'තැපැල් සේවාදායකය';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ප්රොටොකෝලය වලංගු නැත.';
+$lang['mail_retrieval_remote_username'] = 'දුරස්ථ පරිශීලක නාමය';
+$lang['mail_retrieval_server'] = 'සේවාදායකය';
+$lang['mail_retrieval_server_invalid'] = 'සේවාදායකය වලංගු නැත.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL ප්රකාරය';
