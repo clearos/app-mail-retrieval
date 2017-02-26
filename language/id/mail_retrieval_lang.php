787f4e8f6f0466e5df1eecc9b819fb37e69a34c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'SEBUAH LETUSAN';
+$lang['mail_retrieval_app_description'] = 'The Mail Retrieval aplikasi dapat digunakan untuk mengambil email dari eksternal POP dan IMAP server.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Deteksi otomatis';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Terus Jauh Server';
+$lang['mail_retrieval_local_username'] = 'Nama lokal';
+$lang['mail_retrieval_local_username_invalid'] = 'nama lokal tidak valid.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Masuk';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol tidak valid.';
+$lang['mail_retrieval_remote_username'] = 'terpencil Nama';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server tidak valid.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Modus';
