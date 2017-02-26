9adb7e91a47ff0a14a2cc9df881a3a248c408aba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:33:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplicația Mail de preluare poate fi folosit pentru a prelua mail de la POP și IMAP servere externe.';
+$lang['mail_retrieval_app_name'] = 'e-mail de preluare';
+$lang['mail_retrieval_auto_detect'] = 'Auto detectare';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Păstrați pe server de la distanță';
+$lang['mail_retrieval_local_username'] = 'locale Nume utilizator';
+$lang['mail_retrieval_local_username_invalid'] = 'Nume utilizator locală este nevalid.';
+$lang['mail_retrieval_mail_entries'] = 'Intrările e-mail';
+$lang['mail_retrieval_mail_entry'] = 'Intrarea e-mail';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol este nevalid.';
+$lang['mail_retrieval_remote_username'] = 'Nume de utilizator la distanță';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server este nevalid.';
+$lang['mail_retrieval_ssl_mode'] = 'Modul SSL';
