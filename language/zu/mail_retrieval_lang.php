3037e2f50a8ce141a6bda837cf902ed0cec24343
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Insiza Mail Ukubuyiswa ingasetshenziswa ukulanda imeyili evela ezinhlelweni POP kanye IMAP amaseva.';
+$lang['mail_retrieval_app_name'] = 'Mail Ukubuyiswa';
+$lang['mail_retrieval_auto_detect'] = 'ngokuzenzakalela';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Qhubekani Server Remote';
+$lang['mail_retrieval_local_username'] = 'Igama lomsebenzisi Local';
+$lang['mail_retrieval_local_username_invalid'] = 'igama lomsebenzisi Local ayivumelekile.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Umncintiswano';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol ayivumelekile.';
+$lang['mail_retrieval_remote_username'] = 'Igama lomsebenzisi Remote';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ayivumelekile.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
