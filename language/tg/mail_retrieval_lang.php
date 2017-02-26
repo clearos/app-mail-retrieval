b809cd9b9b3b10eea90b15eec79d54eadd7444cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Дар барнома Mail Ҷустуҷӯи метавонад истифода шавад барои Чоргултеппа почта аз POP ва IMAP серверҳои берунӣ.';
+$lang['mail_retrieval_app_name'] = 'Mail ҷустуҷӯ';
+$lang['mail_retrieval_auto_detect'] = 'худкор Ошкор';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Минбаъд низ дурдаст Server';
+$lang['mail_retrieval_local_username'] = 'Логин маҳаллӣ';
+$lang['mail_retrieval_local_username_invalid'] = 'номи корбарӣ ва маҳаллӣ нодуруст аст.';
+$lang['mail_retrieval_mail_entries'] = 'вурудоти Mail';
+$lang['mail_retrieval_mail_entry'] = 'Дохилшавӣ Mail';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Протоколи нодуруст аст.';
+$lang['mail_retrieval_remote_username'] = 'Логин дурдаст';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server нодуруст аст.';
+$lang['mail_retrieval_ssl_mode'] = 'Ҳолати SSL';
