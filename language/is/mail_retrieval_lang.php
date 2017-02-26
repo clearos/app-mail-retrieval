238b958b3adf3782a97fcd43ae0f18632ed70753
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:29:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail sókn app er hægt að nota til að ná póst frá utanaðkomandi POP og IMAP framreiðslumaður.';
+$lang['mail_retrieval_app_name'] = 'mail sókn';
+$lang['mail_retrieval_auto_detect'] = 'sjálfvirkt';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Halda á ytri miðlara';
+$lang['mail_retrieval_local_username'] = 'Local Notandanafn';
+$lang['mail_retrieval_local_username_invalid'] = 'Local notandanafn er ógilt.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Siðareglur er ógild.';
+$lang['mail_retrieval_remote_username'] = 'Remote Notandanafn';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server er ógilt.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
