bfa6b499d0b9958c83a788c42e0b8255855df632
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The app Mail katengedwe angagwiritsidwe ntchito kukatunga makalata ochokera kunja Pop ndi IMAP maseva.';
+$lang['mail_retrieval_app_name'] = 'Mail katengedwe';
+$lang['mail_retrieval_auto_detect'] = 'Auto azindikire';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Pitirizani Server Akutali';
+$lang['mail_retrieval_local_username'] = 'Lolowera Local';
+$lang['mail_retrieval_local_username_invalid'] = 'lolowera Local ndi opanda pake.';
+$lang['mail_retrieval_mail_entries'] = 'Mail zolemba';
+$lang['mail_retrieval_mail_entry'] = 'Mail kulowa';
+$lang['mail_retrieval_mail_server'] = 'Server Mail';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol ndi opanda pake.';
+$lang['mail_retrieval_remote_username'] = 'akumidzi Lolowera';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ndi opanda pake.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mafilimu angaphunzitse';
