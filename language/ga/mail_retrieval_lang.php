3dfbbd1c6bccb8659437f0eb062f79f02ab869b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Is féidir leis an app Mail Aisghabháil a úsáid ríomhphoist chun gabháil ó fhreastalaithe POP agus IMAP seachtracha.';
+$lang['mail_retrieval_app_name'] = 'Mail Aisghabháil';
+$lang['mail_retrieval_auto_detect'] = 'Auto Braith';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Coinnigh ar Fhreastalaí cianda';
+$lang['mail_retrieval_local_username'] = 'Ainm Úsáideora áitiúil';
+$lang['mail_retrieval_local_username_invalid'] = 'Is ainm úsáideora áitiúil neamhbhailí.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Iontrálacha';
+$lang['mail_retrieval_mail_entry'] = 'Mail Iontráil';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Is prótacal neamhbhailí.';
+$lang['mail_retrieval_remote_username'] = 'Ainm Úsáideora cianda';
+$lang['mail_retrieval_server'] = 'freastalaí';
+$lang['mail_retrieval_server_invalid'] = 'Is freastalaí neamhbhailí.';
+$lang['mail_retrieval_ssl_mode'] = 'Mód SSL';
