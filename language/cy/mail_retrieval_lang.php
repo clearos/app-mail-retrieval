28efbf78ca7e0a1bc560b5ddcaa5593c464c7509
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Gall y app Mail Adfer yn cael ei ddefnyddio i nôl post o POP a IMAP gweinyddwyr allanol.';
+$lang['mail_retrieval_app_name'] = 'Mail Adfer';
+$lang['mail_retrieval_auto_detect'] = 'Canfod yn Ymysgogol';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Cadwch ar Server Remote';
+$lang['mail_retrieval_local_username'] = 'Enw defnyddiwr lleol';
+$lang['mail_retrieval_local_username_invalid'] = 'enw defnyddiwr lleol yn annilys.';
+$lang['mail_retrieval_mail_entries'] = 'Entries Mail';
+$lang['mail_retrieval_mail_entry'] = 'Mail Mynediad';
+$lang['mail_retrieval_mail_server'] = 'Gweinydd Mail';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol yn annilys.';
+$lang['mail_retrieval_remote_username'] = 'Pell Enw Defnyddiwr';
+$lang['mail_retrieval_server'] = 'gweinydd';
+$lang['mail_retrieval_server_invalid'] = 'Gweinydd yn annilys.';
+$lang['mail_retrieval_ssl_mode'] = 'Modd SSL';
