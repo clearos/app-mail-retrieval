1fb804c8013c7ca39bfe80269fcba3aae3634758
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail Retrieval app kan brukes til å hente e-post fra eksterne POP og IMAP-servere.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Automatisk gjenkjenning';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Hold på ekstern server';
+$lang['mail_retrieval_local_username'] = 'lokal Brukernavn';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokal brukernavn er ugyldig.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokollen er ugyldig.';
+$lang['mail_retrieval_remote_username'] = 'Remote Brukernavn';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server er ugyldig.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL-modus';
