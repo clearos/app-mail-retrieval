07c765cca6f127df22ec585749b397c1d27fa849
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Захидал Уучлаарай таны хандсан апп гадаад POP ба IMAP сервер захидал авч ашиглаж болно.';
+$lang['mail_retrieval_app_name'] = 'Захидал Уучлаарай таны хандсан';
+$lang['mail_retrieval_auto_detect'] = 'Автоматаар илрүүлэх';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Алсын Server дээр байлга';
+$lang['mail_retrieval_local_username'] = 'Орон нутгийн Хэрэглэгчийн нэр';
+$lang['mail_retrieval_local_username_invalid'] = 'Орон нутгийн нэр хүчин төгөлдөр бус байна.';
+$lang['mail_retrieval_mail_entries'] = 'Захидал Тайлбар';
+$lang['mail_retrieval_mail_entry'] = 'Захидал төгөлдөр болох';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Протокол хүчин төгөлдөр бус байна.';
+$lang['mail_retrieval_remote_username'] = 'алсын Хэрэглэгчийн нэр';
+$lang['mail_retrieval_server'] = 'сервер';
+$lang['mail_retrieval_server_invalid'] = 'Сервер нь хүчин төгөлдөр бус байна.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL горим';
