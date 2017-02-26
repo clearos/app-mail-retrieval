587b34cdfe39904ca512999738d94ce49b656581
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail igbapada app le ti wa ni lo lati bu mail lati ita POP ati IMAP apèsè.';
+$lang['mail_retrieval_app_name'] = 'mail igbapada';
+$lang['mail_retrieval_auto_detect'] = 'auto Ri';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Pa lori jijin Server';
+$lang['mail_retrieval_local_username'] = 'agbegbe olumulo';
+$lang['mail_retrieval_local_username_invalid'] = 'Agbegbe orukọ olumulo ti wa ni invalid.';
+$lang['mail_retrieval_mail_entries'] = 'mail awọn titẹ sii';
+$lang['mail_retrieval_mail_entry'] = 'mail titẹsi';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Bèèrè ni invalid.';
+$lang['mail_retrieval_remote_username'] = 'latọna olumulo';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ni invalid.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL mode';
