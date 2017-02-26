232d2110be9823f28ac0a77a7666183f7b1d6597
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'ਮੇਲ ਪ੍ਰਾਪਤੀ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਬਾਹਰੀ POP ਅਤੇ IMAP ਸਰਵਰ ਤੱਕ ਮੇਲ ਪ੍ਰਾਪਤ ਕਰਨ ਲਈ ਵਰਤਿਆ ਜਾ ਸਕਦਾ ਹੈ.';
+$lang['mail_retrieval_app_name'] = 'ਮੇਲ ਪ੍ਰਾਪਤੀ';
+$lang['mail_retrieval_auto_detect'] = 'ਆਟੋ ਖੋਜ';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'ਰਿਮੋਟ ਸਰਵਰ &#39;ਤੇ ਰੱਖੋ';
+$lang['mail_retrieval_local_username'] = 'ਸਥਾਨਕ ਯੂਜ਼ਰ';
+$lang['mail_retrieval_local_username_invalid'] = 'ਸਥਾਨਕ ਯੂਜ਼ਰ ਗਲਤ ਹੈ.';
+$lang['mail_retrieval_mail_entries'] = 'ਮੇਲ ਇੰਦਰਾਜ਼';
+$lang['mail_retrieval_mail_entry'] = 'ਮੇਲ ਇੰਦਰਾਜ਼';
+$lang['mail_retrieval_mail_server'] = 'ਮੇਲ ਸਰਵਰ';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ਪਰੋਟੋਕਾਲ ਗਲਤ ਹੈ.';
+$lang['mail_retrieval_remote_username'] = 'ਰਿਮੋਟ ਯੂਜ਼ਰ';
+$lang['mail_retrieval_server'] = 'ਸਰਵਰ';
+$lang['mail_retrieval_server_invalid'] = 'ਸਰਵਰ ਗਲਤ ਹੈ.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL ਨੂੰ ਮੋਡ';
