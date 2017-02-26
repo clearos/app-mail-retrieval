2506c45fdfdfb2b0cf9c74b21ea43ad57be0d496
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'แอปอีเมลดึงสามารถใช้ในการดึงข้อมูลอีเมลจาก POP ภายนอกและเซิร์ฟเวอร์ IMAP';
+$lang['mail_retrieval_app_name'] = 'จดหมายดึง';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'เก็บไว้ในเซิร์ฟเวอร์ระยะไกล';
+$lang['mail_retrieval_local_username'] = 'ชื่อผู้ใช้ท้องถิ่น';
+$lang['mail_retrieval_local_username_invalid'] = 'ชื่อผู้ใช้ท้องถิ่นเป็นที่ไม่ถูกต้อง';
+$lang['mail_retrieval_mail_entries'] = 'จดหมายรายการ';
+$lang['mail_retrieval_mail_entry'] = 'จดหมายเข้า';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol เป็นที่ไม่ถูกต้อง';
+$lang['mail_retrieval_remote_username'] = 'ชื่อผู้ใช้ระยะไกล';
+$lang['mail_retrieval_server'] = 'เซิร์ฟเวอร์';
+$lang['mail_retrieval_server_invalid'] = 'เซิร์ฟเวอร์ไม่ถูกต้อง';
+$lang['mail_retrieval_ssl_mode'] = 'โหมด SSL';
