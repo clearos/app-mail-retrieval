a8752fd169156aeb49edb62d71228009ad807cd7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Ang Mail pagkuha app ay maaaring gamitin upang makuha ang mail mula sa panlabas na POP at IMAP server.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Awtomatikong Hanapin';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Panatilihin sa Remote Server';
+$lang['mail_retrieval_local_username'] = 'Lokal Username';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokal username ay hindi wasto.';
+$lang['mail_retrieval_mail_entries'] = 'mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol ay hindi wasto.';
+$lang['mail_retrieval_remote_username'] = 'Remote Username';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server ay hindi wasto.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
