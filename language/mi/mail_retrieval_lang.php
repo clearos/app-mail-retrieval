e03c4a82499e1badecb453f211fc2c7dc820fb6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Ka taea te whakamahi i te taupānga Mail Tiki ki te tiki mēra i POP me te IMAP tūmau waho.';
+$lang['mail_retrieval_app_name'] = 'Mail Tiki';
+$lang['mail_retrieval_auto_detect'] = 'Haurapa aunoa';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Kia mau i runga i Tūmau Mamao';
+$lang['mail_retrieval_local_username'] = 'Ingoa Kaiwhakamahi Local';
+$lang['mail_retrieval_local_username_invalid'] = 'He muhu kaiwhakamahi Local.';
+$lang['mail_retrieval_mail_entries'] = 'Tāurunga mēra';
+$lang['mail_retrieval_mail_entry'] = 'Tāurunga Mail';
+$lang['mail_retrieval_mail_server'] = 'Tūmau mēra';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'He muhu Kawa.';
+$lang['mail_retrieval_remote_username'] = 'Ingoa Kaiwhakamahi mamao';
+$lang['mail_retrieval_server'] = 'tūmau';
+$lang['mail_retrieval_server_invalid'] = 'He muhu Tūmau.';
+$lang['mail_retrieval_ssl_mode'] = 'Aratau SSL';
