ea8947310003e8d924c222c111e654541e98e444
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:27:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Laad app saab kasutada tõmmata mail väliste POP ja IMAP serverid.';
+$lang['mail_retrieval_app_name'] = 'kirjade ülekanne';
+$lang['mail_retrieval_auto_detect'] = 'Automaatne tuvastus';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Hoia kaughaldusserveri';
+$lang['mail_retrieval_local_username'] = 'Kohalik kasutajanime';
+$lang['mail_retrieval_local_username_invalid'] = 'Kohalik kasutajanimi on kehtetu.';
+$lang['mail_retrieval_mail_entries'] = 'mail Sissekanded';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokoll on kehtetu.';
+$lang['mail_retrieval_remote_username'] = 'kaugjuhtimispuldi kasutajanime';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server ei sobi.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL režiimi';
