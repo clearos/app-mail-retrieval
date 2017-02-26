147f0b2ed0203137fc5c317cbc764c6548f95d7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail gyara app za a iya amfani da su kawo mail daga external POP da IMAP sabobin.';
+$lang['mail_retrieval_app_name'] = 'Mail gyara';
+$lang['mail_retrieval_auto_detect'] = 'auto Gane';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Ku Nesa Server';
+$lang['mail_retrieval_local_username'] = 'Local Sunan mai amfani';
+$lang['mail_retrieval_local_username_invalid'] = 'Local sunan mai amfani ba daidai ba ne.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Layinhantsaki ba daidai ba ne.';
+$lang['mail_retrieval_remote_username'] = 'Nesa Sunan mai amfani';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ba daidai ba ne.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
