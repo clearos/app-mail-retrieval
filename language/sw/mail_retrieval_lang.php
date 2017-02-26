f5a9c7b40aee715739cecbf6713e2feb6a3dca5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:34:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'programu Mail Retrieval inaweza kutumika kuchota pepe kutoka nje POP na IMAP seva.';
+$lang['mail_retrieval_app_name'] = 'Barua Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto Kuchunguza';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Kuweka juu ya Server Remote';
+$lang['mail_retrieval_local_username'] = 'Username Mitaa';
+$lang['mail_retrieval_local_username_invalid'] = 'username Mitaa ni batili.';
+$lang['mail_retrieval_mail_entries'] = 'Barua Entries';
+$lang['mail_retrieval_mail_entry'] = 'Barua Entry';
+$lang['mail_retrieval_mail_server'] = 'Server mail';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Itifaki ni batili.';
+$lang['mail_retrieval_remote_username'] = 'kijijini mtumiaji';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server ni batili.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
