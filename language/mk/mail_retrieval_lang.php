efe08d0a6da95d359e7c2dd4f27fcf377e051859
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'стан за Mail Обнова може да се користи за собирање на пошта од надворешни POP и IMAP сервери.';
+$lang['mail_retrieval_app_name'] = 'mail Пребарување';
+$lang['mail_retrieval_auto_detect'] = 'Автоматски детекција';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Имајте на далечински сервер';
+$lang['mail_retrieval_local_username'] = 'локалните име';
+$lang['mail_retrieval_local_username_invalid'] = 'Локалните корисничкото име е невалидно.';
+$lang['mail_retrieval_mail_entries'] = 'mail записи';
+$lang['mail_retrieval_mail_entry'] = 'пошта за влез';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Протоколот не е валиден.';
+$lang['mail_retrieval_remote_username'] = 'Далечински име';
+$lang['mail_retrieval_server'] = 'сервер';
+$lang['mail_retrieval_server_invalid'] = 'Серверот не е валиден.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL режим';
