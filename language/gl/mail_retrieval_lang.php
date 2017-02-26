de11378d304be76d7789acff85e1d0828f13c3a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:27:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'A aplicación Mail Retrieval se pode usar para baixar as mensaxes de servidores POP e IMAP externas.';
+$lang['mail_retrieval_app_name'] = 'obtención de correo';
+$lang['mail_retrieval_auto_detect'] = 'auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Manter o servidor remoto';
+$lang['mail_retrieval_local_username'] = 'Nome de usuario local';
+$lang['mail_retrieval_local_username_invalid'] = 'nome de usuario local é válido.';
+$lang['mail_retrieval_mail_entries'] = 'entradas e-mail';
+$lang['mail_retrieval_mail_entry'] = 'Entrada de correo';
+$lang['mail_retrieval_mail_server'] = 'Servidor de correo';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocolo non é válido.';
+$lang['mail_retrieval_remote_username'] = 'Nome de usuario remoto';
+$lang['mail_retrieval_server'] = 'servidor';
+$lang['mail_retrieval_server_invalid'] = 'Servidor non é válido.';
+$lang['mail_retrieval_ssl_mode'] = 'modo SSL';
