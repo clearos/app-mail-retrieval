12778326572de6ad551ca3ae2e8f0061d7f8591d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:33:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Приложение Почта Retrieval можно использовать для получения почты из внешних POP и IMAP-серверов.';
+$lang['mail_retrieval_app_name'] = 'Почта Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Автоматически определять';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Держите на удаленном сервере';
+$lang['mail_retrieval_local_username'] = 'Местное Имя пользователя';
+$lang['mail_retrieval_local_username_invalid'] = 'Местное имя пользователя является недействительным.';
+$lang['mail_retrieval_mail_entries'] = 'Почтовые записи';
+$lang['mail_retrieval_mail_entry'] = 'Почта Вход';
+$lang['mail_retrieval_mail_server'] = 'Почтовый сервер';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Протокол является недействительным.';
+$lang['mail_retrieval_remote_username'] = 'Удаленный Имя пользователя';
+$lang['mail_retrieval_server'] = 'сервер';
+$lang['mail_retrieval_server_invalid'] = 'Сервер является недействительным.';
+$lang['mail_retrieval_ssl_mode'] = 'Режим SSL';
