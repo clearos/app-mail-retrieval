3b46d1fce17b18e067874002a952a9ed8b6ca866
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'मेल रिट्रिवल अनुप्रयोग बाह्य POP र IMAP सर्भर मेल ल्याउन प्रयोग गर्न सकिन्छ।';
+$lang['mail_retrieval_app_name'] = 'मेल रिट्रिवल';
+$lang['mail_retrieval_auto_detect'] = 'स्वत: पत्ता लगाउने';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'रिमोट सर्भर मा राख्न';
+$lang['mail_retrieval_local_username'] = 'स्थानीय प्रयोगकर्ता नाम';
+$lang['mail_retrieval_local_username_invalid'] = 'स्थानीय प्रयोगकर्ता नाम अवैध छ।';
+$lang['mail_retrieval_mail_entries'] = 'मेल प्रविष्टिहरू';
+$lang['mail_retrieval_mail_entry'] = 'मेल प्रवेश';
+$lang['mail_retrieval_mail_server'] = 'मेल सर्भर';
+$lang['mail_retrieval_pop3'] = 'पप 3';
+$lang['mail_retrieval_protocol_invalid'] = 'प्रोटोकल अमान्य छ।';
+$lang['mail_retrieval_remote_username'] = 'रिमोट प्रयोगकर्ता नाम';
+$lang['mail_retrieval_server'] = 'सर्भर';
+$lang['mail_retrieval_server_invalid'] = 'सर्भर अमान्य छ।';
+$lang['mail_retrieval_ssl_mode'] = 'SSL मोड';
