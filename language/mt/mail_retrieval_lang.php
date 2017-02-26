485c4921f2714249297b7b984ae7d2c2ca0a3993
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Il-app Mail Irkupru jistgħu jintużaw biex jġibu posta minn POP u IMAP servers esterni.';
+$lang['mail_retrieval_app_name'] = 'posta Rkupru';
+$lang['mail_retrieval_auto_detect'] = 'auto Jindunaw';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Żomm fuq server remot';
+$lang['mail_retrieval_local_username'] = 'Username lokali';
+$lang['mail_retrieval_local_username_invalid'] = 'username lokali huwa invalidu.';
+$lang['mail_retrieval_mail_entries'] = 'mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'posta Dħul';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokoll huwa invalidu.';
+$lang['mail_retrieval_remote_username'] = 'Username Remote';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server huwa invalidu.';
+$lang['mail_retrieval_ssl_mode'] = 'Modalità SSL';
