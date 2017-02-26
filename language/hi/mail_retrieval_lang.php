440ff760062f4931cef7ac58e6c5530ee82c7bab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'एक पॉप';
+$lang['mail_retrieval_app_description'] = 'मेल पुनः एप्लिकेशन बाहरी पॉप और IMAP सर्वर से मेल लाने के लिए इस्तेमाल किया जा सकता है।';
+$lang['mail_retrieval_app_name'] = 'मेल पुनः';
+$lang['mail_retrieval_auto_detect'] = 'स्वयंं पता लगाना';
+$lang['mail_retrieval_imap'] = 'आईमैप';
+$lang['mail_retrieval_keep_on_remote_server'] = 'रिमोट सर्वर पर रखें';
+$lang['mail_retrieval_local_username'] = 'स्थानीय यूजर का नाम';
+$lang['mail_retrieval_local_username_invalid'] = 'स्थानीय उपयोगकर्ता नाम अवैध है।';
+$lang['mail_retrieval_mail_entries'] = 'मेल प्रविष्टियां';
+$lang['mail_retrieval_mail_entry'] = 'मेल प्रविष्टि';
+$lang['mail_retrieval_mail_server'] = 'डाक सर्वर';
+$lang['mail_retrieval_pop3'] = 'पॉप 3';
+$lang['mail_retrieval_protocol_invalid'] = 'प्रोटोकॉल अमान्य है।';
+$lang['mail_retrieval_remote_username'] = 'रिमोट यूजर का नाम';
+$lang['mail_retrieval_server'] = 'सर्वर';
+$lang['mail_retrieval_server_invalid'] = 'सर्वर अमान्य है।';
+$lang['mail_retrieval_ssl_mode'] = 'एसएसएल मोड';
