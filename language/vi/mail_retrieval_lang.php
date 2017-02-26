222e1356897f0171be0c51c9c22ed8fcbd488831
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Các ứng dụng Mail Retrieval có thể được sử dụng để lấy mail từ bên ngoài POP và IMAP server.';
+$lang['mail_retrieval_app_name'] = 'thư Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Tự động phát hiện';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Giữ trên Remote Server';
+$lang['mail_retrieval_local_username'] = 'Local Tên đăng nhập';
+$lang['mail_retrieval_local_username_invalid'] = 'tên địa phương là không hợp lệ.';
+$lang['mail_retrieval_mail_entries'] = 'thư mục';
+$lang['mail_retrieval_mail_entry'] = 'thư nhập';
+$lang['mail_retrieval_mail_server'] = 'mail server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Nghị định thư không hợp lệ.';
+$lang['mail_retrieval_remote_username'] = 'Tên đăng nhập từ xa';
+$lang['mail_retrieval_server'] = 'máy chủ';
+$lang['mail_retrieval_server_invalid'] = 'Server là không hợp lệ.';
+$lang['mail_retrieval_ssl_mode'] = 'Chế độ SSL';
