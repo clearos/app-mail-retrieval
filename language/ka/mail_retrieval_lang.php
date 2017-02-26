533ec4db53ef0586ee5964313961d36e20d4e604
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:29:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'ფოსტა ძიება app შეიძლება გამოყენებულ იქნას მოიტანონ mail გარე POP და IMAP სერვერები.';
+$lang['mail_retrieval_app_name'] = 'ფოსტა ძიება';
+$lang['mail_retrieval_auto_detect'] = 'ავტომატური ამოცნობა';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'შეინახეთ დისტანციური Server';
+$lang['mail_retrieval_local_username'] = 'ადგილობრივი ნიკი';
+$lang['mail_retrieval_local_username_invalid'] = 'ადგილობრივი სახელი არასწორია.';
+$lang['mail_retrieval_mail_entries'] = 'ფოსტა შესვლის';
+$lang['mail_retrieval_mail_entry'] = 'ფოსტა Entry';
+$lang['mail_retrieval_mail_server'] = 'ფოსტა სერვერი';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ოქმი არასწორია.';
+$lang['mail_retrieval_remote_username'] = 'დისტანციური ნიკი';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server არასწორია.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL რეჟიმი';
