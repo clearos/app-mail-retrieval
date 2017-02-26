d3e82d7e6cb00fcfcdd376e91640eadf52244d1a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:27:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'ΑΠΟΠ';
+$lang['mail_retrieval_app_description'] = 'Η εφαρμογή Mail Ανάκτηση μπορεί να χρησιμοποιηθεί για να φέρω mail από εξωτερικούς διακομιστές POP και IMAP.';
+$lang['mail_retrieval_app_name'] = 'Mail Ανάκτηση';
+$lang['mail_retrieval_auto_detect'] = 'Αυτόματη αναγνώριση';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Κρατήστε στο απομακρυσμένο διακομιστή';
+$lang['mail_retrieval_local_username'] = 'τοπική Όνομα Χρήστη';
+$lang['mail_retrieval_local_username_invalid'] = 'Τοπικό όνομα χρήστη δεν είναι έγκυρο.';
+$lang['mail_retrieval_mail_entries'] = 'Ενδείξεις Mail';
+$lang['mail_retrieval_mail_entry'] = 'Mail Έναρξη';
+$lang['mail_retrieval_mail_server'] = 'διακομιστή αλληλογραφίας';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Πρωτόκολλο δεν είναι έγκυρο.';
+$lang['mail_retrieval_remote_username'] = 'απομακρυσμένη Όνομα Χρήστη';
+$lang['mail_retrieval_server'] = 'Υπηρέτης';
+$lang['mail_retrieval_server_invalid'] = 'Διακομιστή δεν είναι έγκυρο.';
+$lang['mail_retrieval_ssl_mode'] = 'Λειτουργία SSL';
