1c990e3c30c8c14224e0c5c26c2c2169be287070
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:29:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'メール検索アプリは、外部POPおよびIMAPサーバーからメールをフェッチするために使用することができます。';
+$lang['mail_retrieval_app_name'] = 'メール検索';
+$lang['mail_retrieval_auto_detect'] = '自動検出';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'リモートサーバー上に保管してください';
+$lang['mail_retrieval_local_username'] = 'ローカル・ユーザー名';
+$lang['mail_retrieval_local_username_invalid'] = 'ローカルユーザ名が無効です。';
+$lang['mail_retrieval_mail_entries'] = 'メールエントリ';
+$lang['mail_retrieval_mail_entry'] = 'メールエントリ';
+$lang['mail_retrieval_mail_server'] = 'メールサーバー';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = '議定書は無効です。';
+$lang['mail_retrieval_remote_username'] = 'リモートユーザー名';
+$lang['mail_retrieval_server'] = 'サーバ';
+$lang['mail_retrieval_server_invalid'] = 'サーバーが無効です。';
+$lang['mail_retrieval_ssl_mode'] = 'SSLモード';
