5e6db357abf59bd029e23c5bec926559c1fc716b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:34:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Cijeungjing dimeunangkeun app bisa dipaké pikeun dipulut mail ti luar POP jeung IMAP server.';
+$lang['mail_retrieval_app_name'] = 'surat dimeunangkeun';
+$lang['mail_retrieval_auto_detect'] = 'Otomatis ngadeteksi';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Jieun dina Jauh Server';
+$lang['mail_retrieval_local_username'] = 'Ngaran nu maké lokal';
+$lang['mail_retrieval_local_username_invalid'] = 'username Lokal nyaéta sah.';
+$lang['mail_retrieval_mail_entries'] = 'surat éntri';
+$lang['mail_retrieval_mail_entry'] = 'surat Entry';
+$lang['mail_retrieval_mail_server'] = 'surat Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol téh sah.';
+$lang['mail_retrieval_remote_username'] = 'jauh Username';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server nyaeta sah.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
