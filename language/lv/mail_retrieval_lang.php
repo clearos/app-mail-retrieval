1469bb169da7f39976d6bede0272792b3d236da6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Pasts Ielādes lietotni var izmantot, lai atnest pastu no ārējiem POP un IMAP serveriem.';
+$lang['mail_retrieval_app_name'] = 'pasta ielāde';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Turēt uz Remote Server';
+$lang['mail_retrieval_local_username'] = 'vietējā Lietotājvārds';
+$lang['mail_retrieval_local_username_invalid'] = 'Vietējā lietotājvārds ir nederīgs.';
+$lang['mail_retrieval_mail_entries'] = 'pasta Ieraksti';
+$lang['mail_retrieval_mail_entry'] = 'pasts Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokols ir nederīgs.';
+$lang['mail_retrieval_remote_username'] = 'Remote Lietotājvārds';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ir nederīgs.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
