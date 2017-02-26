bac10b1165beed0a2f0ba99073e510a1043c59ae
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'मेल पुनर्प्राप्ती अॅपला बाह्य POP आणि IMAP सर्व्हर मेल प्राप्त करण्यासाठी वापरले जाऊ शकते.';
+$lang['mail_retrieval_app_name'] = 'मेल पुनर्प्राप्ती';
+$lang['mail_retrieval_auto_detect'] = 'स्वयं शोधा';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'रिमोट सर्व्हर वर ठेवा';
+$lang['mail_retrieval_local_username'] = 'स्थानिक वापरकर्तानाव';
+$lang['mail_retrieval_local_username_invalid'] = 'स्थानिक वापरकर्ता नाव अवैध आहे.';
+$lang['mail_retrieval_mail_entries'] = 'मेल नोंदी';
+$lang['mail_retrieval_mail_entry'] = 'मेल प्रवेश';
+$lang['mail_retrieval_mail_server'] = 'मेल सर्व्हर';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'प्रोटोकॉल अवैध आहे.';
+$lang['mail_retrieval_remote_username'] = 'दूरस्थ वापरकर्तानाव';
+$lang['mail_retrieval_server'] = 'सर्व्हर';
+$lang['mail_retrieval_server_invalid'] = 'सर्व्हर अवैध आहे.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL मोड';
