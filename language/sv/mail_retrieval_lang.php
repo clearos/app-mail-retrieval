6ec6f54bfdecc15e07b704c0163959adf31943e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:34:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Hämtning app kan användas för att hämta e-post från externa POP och IMAP-servrar.';
+$lang['mail_retrieval_app_name'] = 'post Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Automatisk identifiering';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Håll på fjärrserver';
+$lang['mail_retrieval_local_username'] = 'lokal Användarnamn';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokal användarnamn är ogiltigt.';
+$lang['mail_retrieval_mail_entries'] = 'postAnteckningar';
+$lang['mail_retrieval_mail_entry'] = 'post Entry';
+$lang['mail_retrieval_mail_server'] = 'postserver';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokoll är ogiltig.';
+$lang['mail_retrieval_remote_username'] = 'fjärr Användarnamn';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server är ogiltig.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL-läge';
