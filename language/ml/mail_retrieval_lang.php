6001f76db80dd7d8a43c54145b521ee77f99f1c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'മെയിൽ തിരിച്ചെടുക്കല് ​​ബാഹ്യ POP, IMAP സെർവറുകളിൽ നിന്നും മെയിൽ ഉപയോഗിക്കാൻ കഴിയും.';
+$lang['mail_retrieval_app_name'] = 'മെയിൽ തിരിച്ചെടുക്കല്';
+$lang['mail_retrieval_auto_detect'] = 'സ്വയം തിരിച്ചറിയുക';
+$lang['mail_retrieval_imap'] = 'IMAP,';
+$lang['mail_retrieval_keep_on_remote_server'] = 'വിദൂര സെർവറിൽ നിലനിർത്തുക';
+$lang['mail_retrieval_local_username'] = 'പ്രാദേശിക ഉപയോക്തൃനാമം';
+$lang['mail_retrieval_local_username_invalid'] = 'പ്രാദേശിക ഉപയോക്തൃനാമം അസാധുവാണ്.';
+$lang['mail_retrieval_mail_entries'] = 'മെയിൽ എൻട്രികൾ';
+$lang['mail_retrieval_mail_entry'] = 'മെയിൽ എൻട്രി';
+$lang['mail_retrieval_mail_server'] = 'മെയിൽ സെർവർ';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'പ്രോട്ടോക്കോൾ അസാധുവാണ്.';
+$lang['mail_retrieval_remote_username'] = 'വിദൂര ഉപയോക്തൃനാമം';
+$lang['mail_retrieval_server'] = 'സെർവർ';
+$lang['mail_retrieval_server_invalid'] = 'സെർവർ അസാധുവാണ്.';
+$lang['mail_retrieval_ssl_mode'] = 'എസ്എസ്എൽ മോഡ്';
