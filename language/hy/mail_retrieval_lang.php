a7bf9df9a45dc07c1a034b0dc09338bc69dbc543
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:25:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Փոստ Որոնման ծրագիրը կարող է օգտագործվել վերցնել փոստ արտաքին POP and IMAP սերվերների.';
+$lang['mail_retrieval_app_name'] = 'Փոստ որոնում';
+$lang['mail_retrieval_auto_detect'] = 'Ավտո հայտնաբերել';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Շարունակիր հեռավոր սերվերի';
+$lang['mail_retrieval_local_username'] = 'Տեղական Գաղտնաբառ';
+$lang['mail_retrieval_local_username_invalid'] = 'Տեղական Մականուն անվավեր է:';
+$lang['mail_retrieval_mail_entries'] = 'Փոստ Գրառումներ';
+$lang['mail_retrieval_mail_entry'] = 'Փոստ Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Արձանագրությունը անվավեր է:';
+$lang['mail_retrieval_remote_username'] = 'Remote Գաղտնաբառ';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server է անվավեր:';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
