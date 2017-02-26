aa0bfd365c52180c88824c22d163847f8a7fb91f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplikasi Mel Retrieval boleh digunakan untuk mengambil mel daripada POP dan IMAP pelayan luaran.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto Mengesan';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Teruskan Server Jauh';
+$lang['mail_retrieval_local_username'] = 'Nama pengguna tempatan';
+$lang['mail_retrieval_local_username_invalid'] = 'nama pengguna tempatan adalah tidak sah.';
+$lang['mail_retrieval_mail_entries'] = 'mail Penyertaan';
+$lang['mail_retrieval_mail_entry'] = 'Kemasukan mail';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol tidak sah.';
+$lang['mail_retrieval_remote_username'] = 'Nama pengguna Remote';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server tidak sah.';
+$lang['mail_retrieval_ssl_mode'] = 'Mod SSL';
