3d9c6ae63e83e72251d57905cb14537d94719e9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:34:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail app kaqabashada waxaa loo isticmaali karaa si ay u gura mail ka POP iyo IMAP server dibadda.';
+$lang['mail_retrieval_app_name'] = 'Mail kaqabashada';
+$lang['mail_retrieval_auto_detect'] = 'auto ogaashaha';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Ka dhig on Server Remote';
+$lang['mail_retrieval_local_username'] = 'Username Local';
+$lang['mail_retrieval_local_username_invalid'] = 'username Local waa baadil.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Server Mail';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol waa baadil.';
+$lang['mail_retrieval_remote_username'] = 'Username Remote';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server waa baadil.';
+$lang['mail_retrieval_ssl_mode'] = 'Mode SSL';
