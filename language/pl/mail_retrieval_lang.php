14d5c359671a9b673781939cefc107c53a82c223
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplikacja Poczta pobierania może być używany do pobierania poczty z zewnętrznych serwerów POP i IMAP.';
+$lang['mail_retrieval_app_name'] = 'Poczta pobierania';
+$lang['mail_retrieval_auto_detect'] = 'Automatyczne wykrywanie';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
+$lang['mail_retrieval_local_username'] = 'Nazwa lokalna';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokalna nazwa jest niepoprawna.';
+$lang['mail_retrieval_mail_entries'] = 'Poczta Wpisy';
+$lang['mail_retrieval_mail_entry'] = 'Poczta Wejście';
+$lang['mail_retrieval_mail_server'] = 'Serwer poczty elektronicznej';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokół jest nieprawidłowy.';
+$lang['mail_retrieval_remote_username'] = 'Nazwa zdalnego';
+$lang['mail_retrieval_server'] = 'serwer';
+$lang['mail_retrieval_server_invalid'] = 'Serwer jest nieprawidłowy.';
+$lang['mail_retrieval_ssl_mode'] = 'Tryb SSL';
