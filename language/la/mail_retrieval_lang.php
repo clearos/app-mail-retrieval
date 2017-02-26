22b91438950a71757d2355f6be5ae242dfe0afa3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'A app Mail Retrieval mail potest, ut tollerent in IMAP servo, et de externo POP.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto deprehendere';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Custodi in Servo Longinquus';
+$lang['mail_retrieval_local_username'] = 'Local Username';
+$lang['mail_retrieval_local_username_invalid'] = 'Local Username est invalidum.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocollo, invalidum est.';
+$lang['mail_retrieval_remote_username'] = 'Remote Username';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Servo est invalidum.';
+$lang['mail_retrieval_ssl_mode'] = 'Modus SSL';
