cf1e09a150102862f2eea3f0416fe353e36e9170
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'កម្មវិធីសំបុត្រទៅយកអាចត្រូវបានប្រើដើម្បីទៅប្រមូលយកសំបុត្រពីម៉ាស៊ីនបម្រើ POP និង IMAP ខាងក្រៅ។';
+$lang['mail_retrieval_app_name'] = 'ទៅយកសំបុត្រ';
+$lang['mail_retrieval_auto_detect'] = 'រក​ឃើញ​ដោយ​ស្វ័យ​ប្រវត្តិ';
+$lang['mail_retrieval_imap'] = 'IMAP & ‧;';
+$lang['mail_retrieval_keep_on_remote_server'] = 'រក្សាទុកនៅលើម៉ាស៊ីនបម្រើពីចម្ងាយ';
+$lang['mail_retrieval_local_username'] = 'ឈ្មោះអ្នកប្រើក្នុងស្រុក';
+$lang['mail_retrieval_local_username_invalid'] = 'ឈ្មោះអ្នកប្រើក្នុងស្រុកមិនត្រឹមត្រូវ។';
+$lang['mail_retrieval_mail_entries'] = 'ធាតុសំបុត្រ';
+$lang['mail_retrieval_mail_entry'] = 'សំបុត្រចូល';
+$lang['mail_retrieval_mail_server'] = 'ម៉ាស៊ីនបម្រើសំបុត្រ';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ពិធីការមិនត្រឹមត្រូវ។';
+$lang['mail_retrieval_remote_username'] = 'ឈ្មោះអ្នកប្រើពីចម្ងាយ';
+$lang['mail_retrieval_server'] = 'ម៉ាស៊ីនបម្រើ';
+$lang['mail_retrieval_server_invalid'] = 'ម៉ាស៊ីនបម្រើមិនត្រឹមត្រូវ។';
+$lang['mail_retrieval_ssl_mode'] = 'របៀប SSL';
