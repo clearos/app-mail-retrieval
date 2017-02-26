9675bca445c4af267f0099157e2f634f3f786db4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'အဆိုပါမေးလ် retrieval app ကိုပြင်ပ POP နဲ့ IMAP ကိုဆာဗာကနေမေးလ်ဆွဲယူဖို့အသုံးပြုနိုင်ပါသည်။';
+$lang['mail_retrieval_app_name'] = 'မေးလ် retrieval';
+$lang['mail_retrieval_auto_detect'] = 'အော်တိုထောက်လှမ်း';
+$lang['mail_retrieval_imap'] = 'IMAP ကို';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Remote Server ကိုအပေါ် Keep';
+$lang['mail_retrieval_local_username'] = 'ဒေသခံအမည်';
+$lang['mail_retrieval_local_username_invalid'] = 'ဒေသခံအသုံးပြုသူအမည်မမှန်ကန်ဘူး။';
+$lang['mail_retrieval_mail_entries'] = 'မေးလ် Entries';
+$lang['mail_retrieval_mail_entry'] = 'မေးလ် Entry &#39;';
+$lang['mail_retrieval_mail_server'] = 'မေးလ်ဆာဗာ';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ပရိုတိုကောမမှန်ကန်ဘူး။';
+$lang['mail_retrieval_remote_username'] = 'ဝေးလံခေါင်သီအသုံးပြုသူအမည်';
+$lang['mail_retrieval_server'] = 'Server ကို';
+$lang['mail_retrieval_server_invalid'] = 'ဆာဗာမမှန်ကန်ဘူး။';
+$lang['mail_retrieval_ssl_mode'] = 'SSL က Mode ကို';
