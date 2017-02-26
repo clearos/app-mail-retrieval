bf7ab813337940191a34b148f9a335b5fd72e0b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:25:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Axtarış app xarici POP və IMAP server mail almaq üçün istifadə edilə bilər.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detect';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Remote Server edin';
+$lang['mail_retrieval_local_username'] = 'Yerli İstifadəçi adı';
+$lang['mail_retrieval_local_username_invalid'] = 'Yerli istifadəçi adı yanlışdır.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Giriş';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol yanlışdır.';
+$lang['mail_retrieval_remote_username'] = 'Remote İstifadəçi adı';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server yanlışdır.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
