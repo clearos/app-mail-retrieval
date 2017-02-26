66b477d450cda582fb1883e89fda5dcba40cc1d8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:25:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'приложението за електронна поща за теглене може да се използва, за да донесе поща от външни POP и IMAP сървъри.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Автоматично откриване';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Съхранявайте на отдалечения сървър';
+$lang['mail_retrieval_local_username'] = 'Местно име';
+$lang['mail_retrieval_local_username_invalid'] = 'Местно потребителско име е невалиден.';
+$lang['mail_retrieval_mail_entries'] = 'Mail влизания';
+$lang['mail_retrieval_mail_entry'] = 'Mail Влизане';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Протокол е невалиден.';
+$lang['mail_retrieval_remote_username'] = 'дистанционно име';
+$lang['mail_retrieval_server'] = 'Сървър';
+$lang['mail_retrieval_server_invalid'] = 'Сървърът е невалиден.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
