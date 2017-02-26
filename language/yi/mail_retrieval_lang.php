5a097677f42eabf2210dbd1666ef64dfde1d7331
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'אַפּאָפּ';
+$lang['mail_retrieval_app_description'] = 'די פּאָסט ריטריוואַל אַפּ קענען ווערן געניצט צו ברענגען פּאָסט פון פונדרויסנדיק פּאָפּ און ימאַפּ סערווערס.';
+$lang['mail_retrieval_app_name'] = 'פּאָסט ריטריוואַל';
+$lang['mail_retrieval_auto_detect'] = 'אַוטאָ דעטעקט';
+$lang['mail_retrieval_imap'] = 'ימאַפּ';
+$lang['mail_retrieval_keep_on_remote_server'] = 'האַלטן אויף רימאָוט סערווירער';
+$lang['mail_retrieval_local_username'] = 'היגע נאמען';
+$lang['mail_retrieval_local_username_invalid'] = 'היגע נאמען איז פאַרקריפּלט.';
+$lang['mail_retrieval_mail_entries'] = 'מעיל איינסן';
+$lang['mail_retrieval_mail_entry'] = 'מעיל ענטרי';
+$lang['mail_retrieval_mail_server'] = 'מעיל סערווירער';
+$lang['mail_retrieval_pop3'] = 'פּאָפּ 3';
+$lang['mail_retrieval_protocol_invalid'] = 'פּראָטאָקאָל איז פאַרקריפּלט.';
+$lang['mail_retrieval_remote_username'] = 'רימאָוט נאמען';
+$lang['mail_retrieval_server'] = 'סערווירער';
+$lang['mail_retrieval_server_invalid'] = 'סערווירער איז פאַרקריפּלט.';
+$lang['mail_retrieval_ssl_mode'] = 'ססל מאָדע';
