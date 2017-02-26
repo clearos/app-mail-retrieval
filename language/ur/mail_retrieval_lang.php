2d2ac117f2b2343037c15f3c4bdc189e82ffaf17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'میل بازگیری اے پی پی کے خارجی POP اور IMAP سرور سے میل کی بازیافت کے لئے استعمال کیا جا سکتا ہے.';
+$lang['mail_retrieval_app_name'] = 'میل بازگیری';
+$lang['mail_retrieval_auto_detect'] = 'آٹو کا پتہ لگانے';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'ریموٹ سرور پر رکھیں';
+$lang['mail_retrieval_local_username'] = 'مقامی صارف کا نام';
+$lang['mail_retrieval_local_username_invalid'] = 'مقامی صارف نام غلط ہے.';
+$lang['mail_retrieval_mail_entries'] = 'میل میں لکھے';
+$lang['mail_retrieval_mail_entry'] = 'میل انٹری';
+$lang['mail_retrieval_mail_server'] = 'میل سرور';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'پروٹوکول غلط ہے.';
+$lang['mail_retrieval_remote_username'] = 'ریموٹ صارف کا نام';
+$lang['mail_retrieval_server'] = 'سرور';
+$lang['mail_retrieval_server_invalid'] = 'سرور غلط ہے.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL موڈ';
