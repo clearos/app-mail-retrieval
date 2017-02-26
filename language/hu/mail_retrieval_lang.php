521a77ecea19c02e0999c091f0976bbe1db99e0f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'A mail letöltése app lehet letölteni mail külső POP és IMAP szerverek.';
+$lang['mail_retrieval_app_name'] = 'mail letöltése';
+$lang['mail_retrieval_auto_detect'] = 'Auto felismerés';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Tartsuk a távoli szerveren';
+$lang['mail_retrieval_local_username'] = 'helyi Felhasználónév';
+$lang['mail_retrieval_local_username_invalid'] = 'Helyi felhasználónév érvénytelen.';
+$lang['mail_retrieval_mail_entries'] = 'mail bejegyzések';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Jegyzőkönyv érvénytelen.';
+$lang['mail_retrieval_remote_username'] = 'távoli Felhasználónév';
+$lang['mail_retrieval_server'] = 'szerver';
+$lang['mail_retrieval_server_invalid'] = 'Szerver érvénytelen.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL mód';
