68b0a4c4982ca4d0778cbbde66de340d4264aea4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'BİR POP';
+$lang['mail_retrieval_app_description'] = 'Posta Alma uygulaması harici POP ve IMAP sunucularından posta almak için kullanılabilir.';
+$lang['mail_retrieval_app_name'] = 'posta Alma';
+$lang['mail_retrieval_auto_detect'] = 'Otomatik algılama';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Uzaktan Server tutun';
+$lang['mail_retrieval_local_username'] = 'yerel Adı';
+$lang['mail_retrieval_local_username_invalid'] = 'Yerel kullanıcı adı geçersiz.';
+$lang['mail_retrieval_mail_entries'] = 'posta Girişler';
+$lang['mail_retrieval_mail_entry'] = 'posta Girişi';
+$lang['mail_retrieval_mail_server'] = 'Posta sunucusu';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol geçersiz.';
+$lang['mail_retrieval_remote_username'] = 'uzaktan Kullanıcı';
+$lang['mail_retrieval_server'] = 'sunucu';
+$lang['mail_retrieval_server_invalid'] = 'Sunucu geçersiz.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Modu';
