cfa31d1d59050f47b17a343638ea2b764f528cc7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:34:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The app e Mail hopola ka sebelisoa ho lata poso tswang kantle Pop le IMAP server.';
+$lang['mail_retrieval_app_name'] = 'Mail hopola';
+$lang['mail_retrieval_auto_detect'] = 'Auto lemoha';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Le &#39;ne le Server Hōle';
+$lang['mail_retrieval_local_username'] = 'Username Local';
+$lang['mail_retrieval_local_username_invalid'] = 'mosebedisi Local ha o sebetse.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Server Mail';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Melaoana ha o sebetse.';
+$lang['mail_retrieval_remote_username'] = 'ka thōko Username';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ha o sebetse.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
