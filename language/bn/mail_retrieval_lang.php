8ea0f35bda757f4cd6fe13200edf7095ade81d5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:25:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'একটি পপ';
+$lang['mail_retrieval_app_description'] = 'মেল আহরণ অ্যাপ্লিকেশন বাহ্যিক POP এবং IMAP সার্ভারের থেকে মেইল ​​আনতে ব্যবহার করা যেতে পারে.';
+$lang['mail_retrieval_app_name'] = 'মেল উদ্ধার';
+$lang['mail_retrieval_auto_detect'] = 'স্বয়ং সনাক্ত';
+$lang['mail_retrieval_imap'] = 'আইম্যাপ';
+$lang['mail_retrieval_keep_on_remote_server'] = 'দূরবর্তী সার্ভারের উপর রাখুন';
+$lang['mail_retrieval_local_username'] = 'স্থানীয় ব্যবহারকারীর নাম';
+$lang['mail_retrieval_local_username_invalid'] = 'স্থানীয় ব্যবহারকারীর নাম অবৈধ.';
+$lang['mail_retrieval_mail_entries'] = 'মেল দাখিলা';
+$lang['mail_retrieval_mail_entry'] = 'মেল এণ্ট্রি';
+$lang['mail_retrieval_mail_server'] = 'মেইল সার্ভার';
+$lang['mail_retrieval_pop3'] = 'POP 3';
+$lang['mail_retrieval_protocol_invalid'] = 'প্রোটোকল অবৈধ.';
+$lang['mail_retrieval_remote_username'] = 'রিমোট ব্যবহারকারীর নাম';
+$lang['mail_retrieval_server'] = 'সার্ভার';
+$lang['mail_retrieval_server_invalid'] = 'সার্ভার অবৈধ.';
+$lang['mail_retrieval_ssl_mode'] = 'এসএসএল মোড';
