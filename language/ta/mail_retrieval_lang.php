e38a27bda521c38f166bae6507e43653b8d04017
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'மின்னஞ்சல் மீட்டெடுப்பு பயன்பாட்டை வெளி POP மற்றும் IMAP சர்வர்கள் அஞ்சலை எடுக்க பயன்படுத்த முடியும்.';
+$lang['mail_retrieval_app_name'] = 'மின்னஞ்சல் மீட்டெடுப்பு';
+$lang['mail_retrieval_auto_detect'] = 'தானியங்கு கண்டறிதல்';
+$lang['mail_retrieval_imap'] = 'ஐஎம்ஏபி';
+$lang['mail_retrieval_keep_on_remote_server'] = 'தொலை சேவையகத்தில் வைத்து';
+$lang['mail_retrieval_local_username'] = 'உள்ளூர் பயனர் பெயர்';
+$lang['mail_retrieval_local_username_invalid'] = 'உள்ளூர் பயனர் பெயர் தவறானது.';
+$lang['mail_retrieval_mail_entries'] = 'மின்னஞ்சல் பதிவுகள்';
+$lang['mail_retrieval_mail_entry'] = 'மின்னஞ்சல் நுழைவு';
+$lang['mail_retrieval_mail_server'] = 'மின்னஞ்சல் சேவகன்';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'நெறிமுறை தவறானது.';
+$lang['mail_retrieval_remote_username'] = 'தொலை பயனர் பெயர்';
+$lang['mail_retrieval_server'] = 'சர்வர்';
+$lang['mail_retrieval_server_invalid'] = 'சர்வர் தவறானது.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL முறையில்';
