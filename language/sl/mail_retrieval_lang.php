5211676fb949dbc51ecaee1803055c2cfd46f267
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:33:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplikacija Mail duše se lahko uporablja v puščati pošte iz zunanjih strežnikov POP in IMAP.';
+$lang['mail_retrieval_app_name'] = 'mail duše';
+$lang['mail_retrieval_auto_detect'] = 'Samodejno zaznavanje';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Obdržati na oddaljenem strežniku';
+$lang['mail_retrieval_local_username'] = 'lokalno ime';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokalno ime je neveljavna.';
+$lang['mail_retrieval_mail_entries'] = 'mail vnosi';
+$lang['mail_retrieval_mail_entry'] = 'mail Začetek';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol je neveljavna.';
+$lang['mail_retrieval_remote_username'] = 'Daljinski ime';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server je neveljavna.';
+$lang['mail_retrieval_ssl_mode'] = 'Način SSL';
