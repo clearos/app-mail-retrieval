3f6f0c33b4ee209c3fa49d921eb985979db00d8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'EEN KNAL';
+$lang['mail_retrieval_app_description'] = 'The Mail Retrieval app kan worden gebruikt om e-mail te halen uit externe POP- en IMAP-servers.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Automatische detectie';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
+$lang['mail_retrieval_local_username'] = 'lokale Gebruikersnaam';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokale gebruikersnaam is ongeldig.';
+$lang['mail_retrieval_mail_entries'] = 'mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol is ongeldig.';
+$lang['mail_retrieval_remote_username'] = 'Remote Gebruikersnaam';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server is ongeldig.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
