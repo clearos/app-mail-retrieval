73f644f8b9e316f8ca6e16e1053cb7f1d1fab965
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:27:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Retrieval sovellusta voidaan käyttää hakemaan postia ulkoisen POP ja IMAP palvelimia.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Automaattinen havainnointi';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Pidä Etäpalvelimen';
+$lang['mail_retrieval_local_username'] = 'paikallinen Käyttäjätunnus';
+$lang['mail_retrieval_local_username_invalid'] = 'Paikallinen käyttäjätunnus on virheellinen.';
+$lang['mail_retrieval_mail_entries'] = 'mail Viestit';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol on virheellinen.';
+$lang['mail_retrieval_remote_username'] = 'kauko Käyttäjätunnus';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server ei kelpaa.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL-tila';
