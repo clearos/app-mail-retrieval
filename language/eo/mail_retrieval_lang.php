580088cdaa84b35598af6bc0b9b3715a8bb2f7c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:27:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'La Poŝto Retrieval app povas esti uzata por preni poŝton de eksteraj POPO kaj IMAP serviloj.';
+$lang['mail_retrieval_app_name'] = 'poŝto Rehavigo';
+$lang['mail_retrieval_auto_detect'] = 'aŭtomate Eltrovi';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Daŭre Izolita Servilo';
+$lang['mail_retrieval_local_username'] = 'lokaj Uzulnomo';
+$lang['mail_retrieval_local_username_invalid'] = 'Lokaj uzantonomo estas nevalida.';
+$lang['mail_retrieval_mail_entries'] = 'poŝto Eniroj';
+$lang['mail_retrieval_mail_entry'] = 'poŝto Eniro';
+$lang['mail_retrieval_mail_server'] = 'Poŝto Servilo';
+$lang['mail_retrieval_pop3'] = 'POPO3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokolo estas nevalida.';
+$lang['mail_retrieval_remote_username'] = 'fora Uzulnomo';
+$lang['mail_retrieval_server'] = 'servilo';
+$lang['mail_retrieval_server_invalid'] = 'Servilo estas nevalida.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
