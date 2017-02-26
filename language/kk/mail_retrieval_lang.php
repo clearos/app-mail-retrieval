52a2566b78e43d4fdae399f60e7fbde731a9a3d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:29:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Поштаны алуға Қолданба сыртқы POP және IMAP серверлер поштаны алу үшін пайдалануға болады.';
+$lang['mail_retrieval_app_name'] = 'поштаны алуға';
+$lang['mail_retrieval_auto_detect'] = 'Автоанықтау';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Қашықтағы серверге ұстаңыз';
+$lang['mail_retrieval_local_username'] = 'жергілікті Пайдаланушы аты';
+$lang['mail_retrieval_local_username_invalid'] = 'Жергілікті пайдаланушы аты жарамсыз.';
+$lang['mail_retrieval_mail_entries'] = 'Mail жазбалар';
+$lang['mail_retrieval_mail_entry'] = 'пошта жазбасын';
+$lang['mail_retrieval_mail_server'] = 'Пошта сервері';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Хаттама жарамсыз болып табылады.';
+$lang['mail_retrieval_remote_username'] = 'Қашықтан Пайдаланушы аты';
+$lang['mail_retrieval_server'] = 'Сервер';
+$lang['mail_retrieval_server_invalid'] = 'Сервер жарамсыз болып табылады.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL режимі';
