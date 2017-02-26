d11f373c53eb92e30bae2f3edf86ec4084af3070
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:36:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Yuklash ilova tashqi POP va IMAP serverlardan mail olish uchun foydalanish mumkin.';
+$lang['mail_retrieval_app_name'] = 'mail Yuklash';
+$lang['mail_retrieval_auto_detect'] = 'Avto aniqlash';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Masofadan Server davom';
+$lang['mail_retrieval_local_username'] = 'mahalliy Login';
+$lang['mail_retrieval_local_username_invalid'] = 'Mahalliy foydalanuvchi nomi haqiqiy emas.';
+$lang['mail_retrieval_mail_entries'] = 'mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail kirishi';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol haqiqiy emas.';
+$lang['mail_retrieval_remote_username'] = 'masofadan Login';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server haqiqiy emas.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
