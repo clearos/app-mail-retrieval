ba937b4afd77e66b5135dea4a958ab030d4bb6a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'L&#39;aplicació de recuperació de correu electrònic es pot utilitzar per aconseguir el correu des de servidors POP i IMAP externs.';
+$lang['mail_retrieval_app_name'] = 'Recuperació de correu';
+$lang['mail_retrieval_auto_detect'] = 'Auto detectar';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Mantingui el servidor remot';
+$lang['mail_retrieval_local_username'] = 'Nom d&#39;usuari local';
+$lang['mail_retrieval_local_username_invalid'] = 'nom d&#39;usuari local no és vàlid.';
+$lang['mail_retrieval_mail_entries'] = 'entrades electrònic';
+$lang['mail_retrieval_mail_entry'] = 'Entrada electrònic';
+$lang['mail_retrieval_mail_server'] = 'servidor de correu';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol no és vàlid.';
+$lang['mail_retrieval_remote_username'] = 'Nom d&#39;usuari remot';
+$lang['mail_retrieval_server'] = 'servidor';
+$lang['mail_retrieval_server_invalid'] = 'Servidor no és vàlid.';
+$lang['mail_retrieval_ssl_mode'] = 'Mode SSL';
