24c2e0c8257b0b1a8bbbeb82bbd7ac434599939f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:29:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'The Mail nlaghachie ngwa nwere ike iji ozi kpọtara mail si mpụta POP na IMAP sava.';
+$lang['mail_retrieval_app_name'] = 'mail nlaghachie';
+$lang['mail_retrieval_auto_detect'] = 'Auto ịchọpụta';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Nọgidenụ na-Remote Server';
+$lang['mail_retrieval_local_username'] = 'Obodo Aha ojii';
+$lang['mail_retrieval_local_username_invalid'] = 'Obodo aha njirimara bụ ghara ịdị irè.';
+$lang['mail_retrieval_mail_entries'] = 'mail Ndenye';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol bụ ghara ịdị irè.';
+$lang['mail_retrieval_remote_username'] = 'N&#39;ime obodo Aha ojii';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server bụ ghara ịdị irè.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL mode';
