6a4f0171dfb7d7a7b9bd731ac3823968808d94f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:35:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'మెయిల్ శోధన అనువర్తనం బాహ్య POP మరియు IMAP మెయిల్ పొందడంలో ఉపయోగించవచ్చు.';
+$lang['mail_retrieval_app_name'] = 'మెయిల్ శోధన';
+$lang['mail_retrieval_auto_detect'] = 'దానంతట అదే గుర్తించగలిగిన';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'రిమోట్ సర్వర్ చేస్తూనే';
+$lang['mail_retrieval_local_username'] = 'స్థానిక యూజర్ పేరు';
+$lang['mail_retrieval_local_username_invalid'] = 'స్థానిక వాడుకరిపేరు చెల్లదు.';
+$lang['mail_retrieval_mail_entries'] = 'మెయిల్ ఎంట్రీలు';
+$lang['mail_retrieval_mail_entry'] = 'మెయిల్ ఎంట్రీ';
+$lang['mail_retrieval_mail_server'] = 'మెయిల్ సర్వర్';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ప్రోటోకాల్ చెల్లదు.';
+$lang['mail_retrieval_remote_username'] = 'రిమోట్ యూజర్ పేరు';
+$lang['mail_retrieval_server'] = 'సర్వర్';
+$lang['mail_retrieval_server_invalid'] = 'సర్వర్ చెల్లదు.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL మోడ్';
