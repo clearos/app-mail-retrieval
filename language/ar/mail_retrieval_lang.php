39b8662cef806610923f31a4e81bcd8f9714e55a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:24:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'أبوب';
+$lang['mail_retrieval_app_description'] = 'تطبيق البريد استرجاع يمكن استخدامها لجلب البريد من خوادم POP و IMAP الخارجية.';
+$lang['mail_retrieval_app_name'] = 'البريد استرجاع';
+$lang['mail_retrieval_auto_detect'] = 'اكتشاف تلقائي';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'تبقي على الملقم البعيد';
+$lang['mail_retrieval_local_username'] = 'اسم المستخدم المحلي';
+$lang['mail_retrieval_local_username_invalid'] = 'اسم المستخدم المحلي غير صالح.';
+$lang['mail_retrieval_mail_entries'] = 'مقالات الإلكتروني';
+$lang['mail_retrieval_mail_entry'] = 'دخول البريد';
+$lang['mail_retrieval_mail_server'] = 'خادم البريد';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'البروتوكول هو غير صالح.';
+$lang['mail_retrieval_remote_username'] = 'اسم المستخدم عن بعد';
+$lang['mail_retrieval_server'] = 'الخادم';
+$lang['mail_retrieval_server_invalid'] = 'الملقم غير صالح.';
+$lang['mail_retrieval_ssl_mode'] = 'وضع SSL';
