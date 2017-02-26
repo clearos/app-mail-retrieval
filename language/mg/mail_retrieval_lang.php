4d4f7deda523eff5f3b886ed2b0747e76828c239
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:31:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Ny Mail retrieval fampiharana azo ampiasaina mba haka namany Sary avy amin&#39;ny any ivelany sy ny IMAP POP lohamilina.';
+$lang['mail_retrieval_app_name'] = 'Mail retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto mamantatra';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Foana Remote Server';
+$lang['mail_retrieval_local_username'] = 'Local Username';
+$lang['mail_retrieval_local_username_invalid'] = 'Eo an-toerana dia tsy manan-kery solonanarana.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protocol dia tsy manan-kery.';
+$lang['mail_retrieval_remote_username'] = 'Remote Username';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server dia tsy manan-kery.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
