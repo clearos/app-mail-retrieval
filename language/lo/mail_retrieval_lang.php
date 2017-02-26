c4af10c59fb75265428f63bcc9fd35ba74d3c768
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:30:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'ເວັບບອດ';
+$lang['mail_retrieval_app_description'] = 'ການ app Mail Retrieval ສາມາດຖືກນໍາໃຊ້ເພື່ອດຶງຂໍ້ມູນ mail ຈາກພາຍນອກ POP ແລະ IMAP ເຄື່ອງແມ່ຂ່າຍ.';
+$lang['mail_retrieval_app_name'] = 'mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'ອັດຕະໂນມັດການກວດສອບ';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'ໃຫ້ເກັບຮັກສາໃນໄລຍະໄກ Server';
+$lang['mail_retrieval_local_username'] = 'ຊື່ຜູ້ໃຊ້ພາຍໃນປະເທດ';
+$lang['mail_retrieval_local_username_invalid'] = 'ຊື່ຜູ້ໃຊ້ໃນທ້ອງຖິ່ນແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['mail_retrieval_mail_entries'] = 'mail ການອອກສຽງ';
+$lang['mail_retrieval_mail_entry'] = 'mail Entry';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'ອະນຸສັນຍາແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['mail_retrieval_remote_username'] = 'ຊື່ຜູ້ໃຊ້ຫ່າງໄກສອກຫຼີກ';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'ເຄື່ອງແມ່ຂ່າຍແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['mail_retrieval_ssl_mode'] = 'Mode SSL';
