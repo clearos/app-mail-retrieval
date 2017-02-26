89177fdd7bff2fcc78aa27e722b8d631320659f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:28:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Ka app a Mail Rekipere dwe itilize a al pran mail ki soti nan POP ak imap serveurs ekstèn.';
+$lang['mail_retrieval_app_name'] = 'mail prelevman';
+$lang['mail_retrieval_auto_detect'] = 'Auto Detekte';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Kenbe sou sèvè Remote';
+$lang['mail_retrieval_local_username'] = 'D &#39;Lokal';
+$lang['mail_retrieval_local_username_invalid'] = 'non itilizatè lokal se valab.';
+$lang['mail_retrieval_mail_entries'] = 'mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'mail Antre';
+$lang['mail_retrieval_mail_server'] = 'mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Pwotokòl se valab.';
+$lang['mail_retrieval_remote_username'] = 'Remote Username';
+$lang['mail_retrieval_server'] = 'sèvè';
+$lang['mail_retrieval_server_invalid'] = 'Sèvè se valab.';
+$lang['mail_retrieval_ssl_mode'] = 'Mode SSL';
