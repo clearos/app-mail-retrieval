13eaabea2714d1891d519090f4b918d84bf61e25
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = '该邮件检索应用程序可用于外部POP和IMAP服务器收取邮件。';
+$lang['mail_retrieval_app_name'] = '邮件检索';
+$lang['mail_retrieval_auto_detect'] = '自动侦测';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = '保留远程服务器上';
+$lang['mail_retrieval_local_username'] = '本地用户名';
+$lang['mail_retrieval_local_username_invalid'] = '本地用户名无效。';
+$lang['mail_retrieval_mail_entries'] = '邮件报名';
+$lang['mail_retrieval_mail_entry'] = '邮寄入境';
+$lang['mail_retrieval_mail_server'] = '邮件服务器';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = '协议是无效的。';
+$lang['mail_retrieval_remote_username'] = '远程用户名';
+$lang['mail_retrieval_server'] = '服务器';
+$lang['mail_retrieval_server_invalid'] = '服务器是无效的。';
+$lang['mail_retrieval_ssl_mode'] = 'SSL模式';
