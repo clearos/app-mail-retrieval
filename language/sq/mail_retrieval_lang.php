58b67cbe1ba512cd7346e4d70eb260343f696e6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:24:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Aplikacioni Mail rikthim mund të përdoret për të shkoj të marr postën nga serverat POP dhe IMAP jashtme.';
+$lang['mail_retrieval_app_name'] = 'Mail rikthim';
+$lang['mail_retrieval_auto_detect'] = 'Auto zbuluar';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Keep on Remote Server';
+$lang['mail_retrieval_local_username'] = 'Username lokale';
+$lang['mail_retrieval_local_username_invalid'] = 'emrin lokal është i pavlefshëm.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Entries';
+$lang['mail_retrieval_mail_entry'] = 'Mail Hyrja';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokolli është i pavlefshëm.';
+$lang['mail_retrieval_remote_username'] = 'Remote Emri i përdoruesit';
+$lang['mail_retrieval_server'] = 'server';
+$lang['mail_retrieval_server_invalid'] = 'Server është i pavlefshëm.';
+$lang['mail_retrieval_ssl_mode'] = 'Mode SSL';
