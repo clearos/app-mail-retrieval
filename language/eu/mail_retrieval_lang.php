0efed046564a568ca61ed7abb713c6f0f3ef088d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:25:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Retrieval aplikazioa erabili daiteke posta jasotzean kanpoko POP eta IMAP zerbitzariak from.';
+$lang['mail_retrieval_app_name'] = 'Mail Retrieval';
+$lang['mail_retrieval_auto_detect'] = 'Auto detektatu';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Urruneko Server on Jarrai';
+$lang['mail_retrieval_local_username'] = 'Bertako Username';
+$lang['mail_retrieval_local_username_invalid'] = 'Tokiko erabiltzaile izena baliogabea da.';
+$lang['mail_retrieval_mail_entries'] = 'Mail sarrerak';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokolo baliogabea da.';
+$lang['mail_retrieval_remote_username'] = 'Urruneko Username';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server baliogabea da.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL Mode';
