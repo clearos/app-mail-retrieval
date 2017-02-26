c91b08687a5fa0a71986fd4c4af8136f4ebf5186
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:32:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'برنامه ایمیل بازیابی می توان برای دریافت نامه از POP و IMAP سرور های خارجی.';
+$lang['mail_retrieval_app_name'] = 'ایمیل بازیابی';
+$lang['mail_retrieval_auto_detect'] = 'تشخیص خودکار';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'نگه دارید در سرور از راه دور';
+$lang['mail_retrieval_local_username'] = 'نام کاربری محلی';
+$lang['mail_retrieval_local_username_invalid'] = 'نام کاربری محلی نامعتبر است.';
+$lang['mail_retrieval_mail_entries'] = 'مطالب ایمیل';
+$lang['mail_retrieval_mail_entry'] = 'ورود ایمیل';
+$lang['mail_retrieval_mail_server'] = 'ایمیل سرور';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'پروتکل نامعتبر است.';
+$lang['mail_retrieval_remote_username'] = 'نام کاربری از راه دور';
+$lang['mail_retrieval_server'] = 'سرور';
+$lang['mail_retrieval_server_invalid'] = 'سرور نامعتبر است.';
+$lang['mail_retrieval_ssl_mode'] = 'حالت SSL';
