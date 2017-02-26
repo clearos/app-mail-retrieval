52037b264e137ea4ee91e29d56afa1b0705a9c8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:24:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Die Mail Herwinning inligting kan gebruik word om pos te gaan haal eksterne POP bedieners.';
+$lang['mail_retrieval_app_name'] = 'Mail Herwinning';
+$lang['mail_retrieval_auto_detect'] = 'Auto Spoor';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Hou op Remote Server';
+$lang['mail_retrieval_local_username'] = 'plaaslike Gebruikersnaam';
+$lang['mail_retrieval_local_username_invalid'] = 'Plaaslike gebruikersnaam is ongeldig.';
+$lang['mail_retrieval_mail_entries'] = 'Mail Inskrywings';
+$lang['mail_retrieval_mail_entry'] = 'Mail Toegang';
+$lang['mail_retrieval_mail_server'] = 'e-pos bediener';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol is ongeldig.';
+$lang['mail_retrieval_remote_username'] = 'Remote Gebruikersnaam';
+$lang['mail_retrieval_server'] = 'bediener';
+$lang['mail_retrieval_server_invalid'] = 'Bediener is ongeldig.';
+$lang['mail_retrieval_ssl_mode'] = 'SSL af';
