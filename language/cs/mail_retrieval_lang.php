df49d86be84a8141ccab06e5ba5c8fddc3aec2d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 15:26:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/mail_retrieval_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['mail_retrieval_apop'] = 'APOP';
+$lang['mail_retrieval_app_description'] = 'Mail Retrieval aplikace může být použita pro získání pošty z externích POP a IMAP servery.';
+$lang['mail_retrieval_app_name'] = 'mailu';
+$lang['mail_retrieval_auto_detect'] = 'Auto detekce';
+$lang['mail_retrieval_imap'] = 'IMAP';
+$lang['mail_retrieval_keep_on_remote_server'] = 'Mějte na vzdálený server';
+$lang['mail_retrieval_local_username'] = 'místní Uživatelské jméno';
+$lang['mail_retrieval_local_username_invalid'] = 'Místní uživatelské jméno je neplatné.';
+$lang['mail_retrieval_mail_entries'] = 'mailové Příspěvky';
+$lang['mail_retrieval_mail_entry'] = 'Mail Entry';
+$lang['mail_retrieval_mail_server'] = 'Mail Server';
+$lang['mail_retrieval_pop3'] = 'POP3';
+$lang['mail_retrieval_protocol_invalid'] = 'Protokol je neplatný.';
+$lang['mail_retrieval_remote_username'] = 'Dálkové Uživatelské jméno';
+$lang['mail_retrieval_server'] = 'Server';
+$lang['mail_retrieval_server_invalid'] = 'Server je neplatný.';
+$lang['mail_retrieval_ssl_mode'] = 'Režim SSL';
